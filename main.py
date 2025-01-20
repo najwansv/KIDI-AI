92e3cdd294cbb345f26bb998c94e867fe76240e4
NajwanSV <najwan.joan.sv@gmail.com>
2025-01-20 15:10:54 +0700

    second commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+# Specify the directory containing your web files
+DIRECTORY = "Dashboard"
+PORT = 8000  # You can choose any port
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+# Start the server
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving HTTP on localhost:{PORT} (http://127.0.0.1:{PORT})")
+    httpd.serve_forever()
